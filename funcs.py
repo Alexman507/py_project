e00f778c8ee62353d61c9d6ea063087cff9e9900
SRE <Nerocross507@gmail.com>
2024-01-22 15:27:24 +0300

    Добавлена функция сложения

---
+++ funcs.py
@@ -0,0 +1,19 @@
+def add():
+    item = int(input("Введите первое число: "))
+    summand = int(input("Введите второе число: "))
+    result = item + summand
+    return result
+
+
+def diff():
+    # инпуты двух числе для вычитания
+    # minuend = int(input("Введите уменьшаемое число: "))
+    # subtrahend = int(input("Введите вычитаемое: "))
+    # разница
+    # diff = minuend - subtrahend
+    # print("Разность равна: ", diff)
+    pass
+
+
+if __name__ == '__main__':
+    print("Сумма равна: ", add())
